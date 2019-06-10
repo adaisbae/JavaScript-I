cfa9bd388e2f8cc129b3bd074759a538bb1cd0a0
AGORA\mjones <mjones@14west.us>
2019-06-10 17:17:53 -0400

    array challenge 4 complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -83,8 +83,13 @@ console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-// let carYears = [];
-// console.log();
+let carYears = [];
+for ( let i = 0; i < inventory.length; i++) {
+    carYears.push(inventory[i].car_year);
+}
+
+
+console.log(carYears);
 
 
 
