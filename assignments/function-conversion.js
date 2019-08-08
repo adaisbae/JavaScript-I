13afdbd4ac49be25d2bb3bb32993548fb1e95c85
adaisbae <melissajoneswrites@gmail.com>
2019-08-07 23:16:30 -0400

    arrow function conversions complete

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -3,22 +3,29 @@
 // let myFunction = function () {
 // console.log("Function was invoked!");
 // };
-// myFunction();
+
+let myFunction = () => console.log("Function was invoked!");
+myFunction();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
-// anotherFunction("Example");
+
+let anotherFunction = word => word;
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
-// add(1,2);
+let add = (num1, num2) => num1 + num2;
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
-// subtract(1,2);
+
+let subtract = (num1, num2) => num1 - num2;
+console.log(subtract(1,2));
 
 
 // Stretch
